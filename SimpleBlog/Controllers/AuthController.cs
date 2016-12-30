7138f158314131aaee96904f5aa8661622337993
adamyi87 <adamyi87@yahoo.com>
2016-12-30 10:25:03 -0600

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,14 @@ namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        [HttpGet]
         public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
         {
             return View();
         }
